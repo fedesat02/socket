f549b148b54d0fc8798057ce302056f44ef82293
fedesat02 <fedesat02@hotmail.com>
2021-03-31 16:22:07 +0200

    simulazione apertura di una connessione

--- client.py
+++ client.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python3
 
-
-input_string = 'Hello'
-print(type(input_string))
-input_bytes_encoded = input_string.encode()
-print(type(input_bytes_encoded))
-print(input_bytes_encoded)
-output_string=input_bytes_encoded.decode()
-print(type(output_string))
-print(output_string)
-
 import socket
 
 SERVER_ADDRESS = '127.0.0.1'
@@ -20,32 +10,29 @@ sock_service = socket.socket()
 sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
 
 print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
+#             send    receive        send              receive
+protocollo = ["SYN", "SYN ACK", "ACK with DATA", "ACK for DATA"]
+step = 0
+dati = str(step)
 while True:
-    try:
-        dati = input("Inserisci i dati da inviare (0 per terminare la connessione): ")
-    except EOFError:
-        print("\nOkay. Exit")
-        break
-    if not dati:
-        print("Non puoi inviare una stringa vuota!")
-        continue
-    if dati == '0':
-        print("Chiudo la connessione con il server!")
-        break
     
     dati = dati.encode()
 
     sock_service.send(dati)
-
+    print("Invio: " + str(step) + " - " + protocollo[step])
     dati = sock_service.recv(2048)
-
     if not dati:
         print("Server non risponde. Exit")
         break
-    
     dati = dati.decode()
-
-    print("Ricevuto dal server:")
-    print(dati + '\n')
-
+    if dati == '3':
+        print("Ricevuto: " + dati + " - " + protocollo[int(dati)])
+        print("Termino connessione")
+        break
+    else:
+        step = int(dati)   
+        print("Ricevuto: " + str(step) + " - " + protocollo[step])
+        step += 1
+        dati = str(step) 
+    
 sock_service.close()
--- server.py
+++ server.py
@@ -16,29 +16,28 @@ sock_listen.bind((SERVER_ADDRESS, SERVER_PORT))
 sock_listen.listen(5)
 
 print("Server in ascolto su %s." % str((SERVER_ADDRESS, SERVER_PORT)))
-
+#             send    receive        send              receive
+protocollo = ["SYN", "SYN ACK", "ACK with DATA", "ACK for DATA"]
 
 while True:
     sock_service, addr_client = sock_listen.accept()
     print("\nConnessione ricevuta da " + str(addr_client))
     print("\nAspetto di ricevere i dati ")
-    contConn=0
+    step=0
     while True:
-        dati = sock_service.recv(2048)
-        contConn+=1
+        dati = sock_service.recv(2048) #riceve i dati e attende la richiesta
         if not dati:
             print("Fine dati dal client. Reset")
             break
-        
+            
         dati = dati.decode()
-        print("Ricevuto: '%s'" % dati)
-        if dati=='0':
-            print("Chiudo la connessione con " + str(addr_client))
-            break
-        dati = "Risposta a : " + str(addr_client) + ". Il valore del contatore è : " + str(contConn)
+        step = int(dati)
+        print("Ricevuto: " + dati + " - " + protocollo[step])
+        step += 1
+        dati = str(step)
 
         dati = dati.encode()
-
+        print("Invio: " + str(step) + " - " + protocollo[step])
         sock_service.send(dati)
 
     sock_service.close()
